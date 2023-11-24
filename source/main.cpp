6b43c3f56e35cf78d087fa3620f4a384d0a7c824
Caio Couto <caiocouto25@hotmail.com>
2023-11-23 21:29:38 -0300

    modified:   source/main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -2,23 +2,28 @@
 
 int main(int argc, char** argv){
 
-    /* checks if any argv was given at execution time
-    if not, asks the user to input someone */
+    /*
+        checks if any argv was given at execution time
+        if not, asks the user to input someone
+    */
     if(argc <= 1){
-        char buffer[20]; // buffer only to down 20 chars, v1 qrcode limit
+        char buffer[20]; // buffer only to down 19 chars + '\n' , v1 qrcode limit
         std::cout<<"You have to pass data to be encoded\n";
         std::cout<<"e.g: \".\\main.exe samplestring\"\n";
-        std::cin>>buffer; // unsafe approach
+        std::cin>> std::setw(19) >> buffer; // setw(19) limits the size of data input
         *(argv+1) = buffer; // places the buffer on same adress as argv+1, i think it leads to less code
     }
 
     Image out(21, 21, 1); // Creates an Image object
-
-    /* cals the generate function,
-    this function contains all other instructions */
+    
     try{
+        /*
+            cals the generate function,
+            this function contains all other instructions
+        */
         out.generate(21, "'Twas brillig", 0b011);
     }catch (...){
+        // pretty sure it isn't a good error handler
         return -1;
     }
     return 0;
